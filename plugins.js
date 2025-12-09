d647ad2333bb59c2f2198002b227dae7ea89ab85
zhiwei <1178679449@qq.com>
2025-12-09 16:51:12 +0800

    变更更新地址

--- plugins.js
+++ plugins.js
@@ -14,8 +14,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=milkywayidle.com
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://raw.githubusercontent.com/xxx/yyy/main/mwimytool.user.js
-// @downloadURL  https://raw.githubusercontent.com/xxx/yyy/main/mwimytool.user.js
+// @updateURL    https://github.com/zhiweiiii/mwi-tool-auto-buy-sell/blob/main/plugins.js
+// @downloadURL  https://github.com/zhiweiiii/mwi-tool-auto-buy-sell/blob/main/plugins.js
 // ==/UserScript==
  
 (function () {
